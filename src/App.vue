ca998b0eba2a843b6ec042656a72631cb1c3da45
ruuto102 <yoshizawanana@yoshizaananoMBP.elecom>
2021-01-29 12:11:39 +0900

    section9.129.eventTitleコンポーネントの追加

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,45 @@
 <keep-alive>
 <component :is="currentComponent"></component>
 </keep-alive>
-<div>
+<div style="padding: 10rem;">
   <h2>イベントのフォーム</h2>
-  <label for="title">タイトル</label>
-  <input id="title" type="text" v-model="eventData.title">
-  <p>{{eventData.title}}</p>
+
+  <label for="maxNumber">最大人数</label>
+  <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
+  <p>{{eventData.maxNumber}}</p>
+
+   <label for="host">主催者</label>
+  <input id="host" type="text" v-model.trim="eventData.host">
+  <p>{{eventData.host}}</p>
+
+   <label for="detail">イベントの内容</label>
+   <textarea name="" id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+    <pre>{{eventData.detail}}</pre>
+
+   <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+   <label for="isPrivate">非公開</label>
+   <p>{{ eventData.isPrivate }}</p>
+
+   <p>参加条件</p>
+   <input type="checkbox" id="10" value="10代"v-model="eventData.taeget">
+   <label for="10">10代</label>
+    <input type="checkbox" id="20" value="20代"v-model="eventData.taeget">
+   <label for="20">20代</label>
+    <input type="checkbox" id="30" value="30代"v-model="eventData.taeget">
+   <label for="30">30代</label>
+   <p>{{ eventData.taeget }}</p>
+
+   <p>参加費</p>
+   <input type="radio" id="free" value="無料" v-model="eventData.price">
+    <label for="free">無料</label>
+     <input type="radio" id="paid" value="有料" v-model="eventData.price">
+    <label for="free">有料</label>
+
+    <p>開催場所</p>
+    <select v-model="eventData.location">
+      <option v-for="location in locations" :key="location">{{ location }}</option>
+    </select>
+    <p>{{ eventData.location }}</p>
 </div>
 </div>
 </template>
@@ -27,9 +61,17 @@ export default {
   data() {
     return {
       number: 10,
+      locations: ["東京","大阪","名古屋"],
       currentComponent: 'About',
       eventData:{
-        title: "タイトル"
+        title: "",
+        maxNumber:0,
+        host: "",
+        detail:"",
+        isPrivate:false,
+        taeget: [],
+        price:"無料",
+        location:"東京"
       }
     };
   },
@@ -42,4 +84,3 @@ export default {
   }
 }
 </script>
-
---
+++ src/components/eventTitle.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <label for="title">タイトル</label>
+    <input id="title" type="text" v-model="eventData.title">
+    <p>{{eventData.title}}</p>
+  </div>
+</template>
