006614797d71464e4c65f34b77f0feb8947fdf93
ruuto102 <ruuto102@gmail.com>
2021-02-16 16:51:02 +0900

    section12.166

--- udemy-vuejs/src/App.vue
+++ udemy-vuejs/src/App.vue
@@ -1,7 +1,12 @@
 <template>
-<div>
+<div class="main">
   <button @click="show = !show">切替え</button>
+  <transition name="fade" appear>
   <p v-if="show">hello</p>
+  </transition>
+  <transition name="slide" appear>
+    <p v-if="show">bye</p>
+  </transition>
 </div>
 </template>
 
@@ -15,7 +20,49 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: 1s;
+}
+.fade-enter-to {
+opacity: 1;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: 1s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-active {
+   animation: slide-in 0.5s;
+   transition: opacity 0.5s;
+}
+.slide-leave-active {
+    animation: slide-in 0.5s reverse;
+   transition: opacity 0.5s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+
 .main {
   width: 70%;
   margin: auto;
